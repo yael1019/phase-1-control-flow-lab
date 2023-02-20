4b3fc1b0d0b607ed51542f54865b6c82c4200e65
Yael Harosh <yaelharosh01@gmail.com>
2023-02-20 13:28:56 -0500

    pass all labs

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num) {
   // Write your code here!
+  if (num <= 400) {
+    return `This one is on me!`;
+  } else if (num > 400 && num <= 2000) {
+    return `That will be twenty bucks.`;
+  } else if (num > 2000 && num < 2500) {
+    return `I will gladly take your thirty bucks.`;
+  }
+  else {
+    return `No can do.`;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city === 'NYC' ? `Ok, sounds good.` : `No go.`;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return `Thank you so much.`;
+      break;
+    case 'not as generous':
+      return `Thank you.`;
+      break;
+    default:
+      return `Bye.`;
+  }
 }
